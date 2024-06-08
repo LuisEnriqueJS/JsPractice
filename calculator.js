50a36d99a85885ea37da55dbd23c32c036b9df19
LuisEnrique <lbermudez@ucol.mx>
2024-06-08 14:05:09 -0500

    Aqui esta la funcion calculadora ya comprobada

--- calculator.js
+++ calculator.js
@@ -1,18 +1,5 @@
-var resultado = operaciones ( 5,6, "resta" )
-var resultado2 = operaciones (10,2, "division")
-var resultado3 = operaciones (2,3, "suma")
-var resultado4 = operaciones (2,3, "potenciaaa")
 
-if (resultado !== undefined) {
-    console.log(resultado);}
-if (resultado2 !== undefined) {
-    console.log(resultado2);}
-if (resultado3 !== undefined) {
-    console.log(resultado3);}
-if (resultado4 !== undefined) {
-    console.log(resultado4);}
-    
-
+calculadora (146,840,"multiplicacion")
 function operaciones (number1,number2, operacion ){
     switch (operacion) {
         case "suma" :
@@ -25,10 +12,17 @@ function operaciones (number1,number2, operacion ){
             return number1 / number2
         case "potencia":
             return Math.pow (number1, number2)
-
         default:
-
             console.log(`Operacion ${operacion} no implementada.`)
             break
+    }
+}
+function calculadora (numberA,numberB,Operacion){
+    var resultado = operaciones(numberA,numberB,Operacion)
+    if (resultado == undefined) {
+        console.log(`La funcion calculadora no es valida`)
+    }
+    else {
+        console.log(`El resultado es ${resultado}`)
     }
 }
